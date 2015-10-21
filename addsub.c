c1f8423317bf2332e8242aaca3896ea5a4d20752
rgupta1220 <rgupta1220@ymail.com>
2015-10-21 16:24:05 +0530

    Create addsub.c
    created c file and added functionality for addition of two numbers

---
+++ addsub.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+int a,b,c;
+printf("Enter first number");
+scanf("%d",&a);
+printf("Enter second number");
+scanf("%d",&b);
+c=a+b;
+printf("sum of two no is  %d", c);
+getch();
+}
